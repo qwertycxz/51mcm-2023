7bd8decd5b64f92190ad0f9484addf2a6b655221
qwertycxz <gmail2@qq.com>
2023-04-28 20:26:14 +0800

    第一问完成

--- B题.r
+++ B题.r
@@ -1,26 +1,66 @@
+library("trend")
+library("plyr")
 # 数据读入
 attachment1 <- read.csv("附件1.csv", encoding = "UTF-8")
 attachment1[, 1] <- as.Date(attachment1[, 1])
 attachment1[, "Day"] <- as.integer(format(attachment1[, 1], "%d"))
 attachment1[, "Month"] <- as.integer(format(attachment1[, 1], "%m"))
 
-# 第一问:未完成
+# 第一问:完成
+trend_pre <- read.csv("附件1-趋势预处理.csv", encoding = "UTF-8")
+# 发出
 deliver <- list()
 deliver_sum <- list()
-deliver_diff <- list()
+# deliver_diff <- list()
+# deliver_trend <- list()
 for (i in LETTERS) {
     deliver[[i]] <- attachment1[attachment1$Delivering == i, ][, "PCS"]
     deliver_sum[i] <- sum(deliver[[i]])
-    deliver_diff[[i]] <- diff(deliver[[i]], 1)
 }
+# 接收
 receive <- list()
 receive_sum <- list()
-reveive_diff <- list()
+# reveive_diff <- list()
+# receive_trend <- list()
 for (i in LETTERS) {
     receive[[i]] <- attachment1[attachment1$Receiving == i, ][, "PCS"]
     receive_sum[i] <- sum(receive[[i]])
-    reveive_diff[[i]] <- diff(receive[[i]], 1)
 }
+# 趋势
+trend <- list()
+trend_p <- list()
+trend_statistic <- list()
+for (i in LETTERS) {
+    trend[[i]] <- mk.test(trend_pre[, i])
+    trend_p[i] <- trend[[i]]["p.value"]
+    trend_statistic[i] <- trend[[i]]["statistic"]
+}
+# 相关
+cov <- list()
+cov_p <- list()
+cov_statistic <- list()
+for (i in LETTERS) {
+    cov[[i]] <- mk.test(diff(trend_pre[, i]))
+    cov_p[i] <- cov[[i]]["p.value"]
+    cov_statistic[i] <- cov[[i]]["statistic"]
+}
+# 熵权法
+topsis <- as.data.frame(cbind(unlist(deliver_sum), unlist(receive_sum), unlist(trend_statistic), unlist(cov_statistic)))
+topsis[is.na(topsis)] <- 0
+entropy_positive <- function(x) {
+    x <- unlist(x)
+    y <- (x - min(x)) / (max(x) - min(x))
+    p <- y / sum(y)
+    entropy <- -1 / log(length(x)) * sum(ifelse(p == 0, 0, p * log(p)))
+    return(entropy)
+}
+entropy_data <- colwise(entropy_positive)(topsis)
+entropy_weight <- (1 - entropy_data) / sum(1 - entropy_data)
+# 标准化
+scaled_data <- scale(topsis)
+write.csv(scaled_data, "附件1-标准化.csv")
+write.csv(entropy_weight, "附件1-权重.csv")
+# 结果见 第一问结果.py
 
 # 第二问
 index <- sample(c(TRUE, FALSE), length(attachment1), replace = TRUE, prob = c(0.75, 0.25))
---
+++ 第一问结果.py
@@ -0,0 +1,17 @@
+from csv import reader
+with open("附件1-权重.csv") as f:
+    read = reader(f)
+    for i in read:
+        try:
+            weight = tuple(map(float, i[1:]))
+        except:
+            pass
+result = {}
+with open("附件1-标准化.csv") as f:
+    read = reader(f)
+    for i in read:
+        if (len(i[0])):
+            result[i[0]] = 0
+            for j in range(4):
+                result[i[0]] += float(i[j + 1]) * weight[j]
+print(sorted(result.items(), key = lambda d: d[1], reverse = True))
